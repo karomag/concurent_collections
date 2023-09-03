f1fbc5d5dc7d942012ba1b0a22fdeaa0937ec33d
karomag <karomag88@mail.ru>
2023-09-03 12:45:07 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,101 @@
+package org.example;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Main {
+
+    public static ArrayBlockingQueue<String> queue1 = new ArrayBlockingQueue<>(100); // очередь по 100 элементов
+    public static ArrayBlockingQueue<String> queue2 = new ArrayBlockingQueue<>(100);
+    public static ArrayBlockingQueue<String> queue3 = new ArrayBlockingQueue<>(100);
+    public static long maxA = 0;
+    public static long maxB = 0;
+    public static long maxC = 0;
+    public static String maxAStr;
+    public static String maxBStr;
+    public static String maxCStr;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread0 = new Thread(() -> {
+            for (int i = 0; i < 10_000; i++) { // 10 000 текстов
+                try {
+                    queue1.put(generateText("abc", 100_000)); // 100 000 символов в строке
+                    queue2.put(generateText("abc", 100_000)); // put добавляет, если может, иначе спит
+                    queue3.put(generateText("abc", 100_000));
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread thread1 = new Thread(() -> {
+            try {
+                String currentStr = queue1.take();
+                long currentMaxA = currentStr.chars().filter(ch -> ch == 'a').count();
+                if (currentMaxA > maxA) {
+                    maxA = currentMaxA;
+                    maxAStr = currentStr;
+                }
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                String currentStr = queue2.take();
+                long currentMaxA = currentStr.chars().filter(ch -> ch == 'b').count();
+                if (currentMaxA > maxB) {
+                    maxB = currentMaxA;
+                    maxBStr = currentStr;
+                }
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            try {
+                String currentStr = queue3.take();
+                long currentMaxA = currentStr.chars().filter(ch -> ch == 'c').count();
+                if (currentMaxA > maxC) {
+                    maxC = currentMaxA;
+                    maxCStr = currentStr;
+                }
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        thread0.start();
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread3.join();
+        thread2.join();
+        thread1.join();
+        thread0.join();
+
+        System.out.println("Максимум a " + maxA + ": " + maxAStr);
+        System.out.println("************************************************************************");
+        System.out.println("Максимум b " + maxB + ": " + maxBStr);
+        System.out.println("************************************************************************");
+        System.out.println("Максимум c " + maxC + ": " + maxCStr);
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+
+}
