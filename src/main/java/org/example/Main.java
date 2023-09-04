dd90bd15449c7dd858ac57ecb5190d8d460ad08b
karomag <karomag88@mail.ru>
2023-09-04 23:44:38 +0300

    refactoring code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,104 +2,59 @@ package org.example;
 
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
     public static ArrayBlockingQueue<String> queue1 = new ArrayBlockingQueue<>(100); // очередь по 100 элементов
     public static ArrayBlockingQueue<String> queue2 = new ArrayBlockingQueue<>(100);
     public static ArrayBlockingQueue<String> queue3 = new ArrayBlockingQueue<>(100);
-    public static long maxA = 0;
-    public static long maxB = 0;
-    public static long maxC = 0;
-    public static String maxAStr;
-    public static String maxBStr;
-    public static String maxCStr;
-
+    public static Thread textsThread;
     public static void main(String[] args) throws InterruptedException {
-
-        Thread thread0 = new Thread(() -> {
+        textsThread = new Thread(() -> {
             for (int i = 0; i < 10_000; i++) { // 10 000 текстов
                 try {
                     queue1.put(generateText("abc", 100_000)); // 100 000 символов в строке
                     queue2.put(generateText("abc", 100_000)); // put добавляет, если может, иначе спит
                     queue3.put(generateText("abc", 100_000));
                     Thread.sleep(100);
-//                    System.out.println("Queues put() " + queue1);
-//                    System.out.println("Queues put() " + queue2);
-//                    System.out.println("Queues put() " + queue3);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
         });
+        textsThread.start();
 
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
+        Thread threadA = getThread(queue1, 'a');
+        Thread threadB = getThread(queue2, 'b');
+        Thread threadC = getThread(queue3, 'c');
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+
+        threadA.join();
+        threadB.join();
+        threadC.join();
+    }
+
+    private static Thread getThread(ArrayBlockingQueue<String> queue, char ch) {
+        AtomicLong maxCh = new AtomicLong();
+        return new Thread(() -> {
+            while (textsThread.isAlive()) {
                 try {
-                    String currentStr = queue1.take();
-                    long currentMaxA = currentStr.chars().filter(ch -> ch == 'a').count();
-                    if (currentMaxA > maxA) {
-                        maxA = currentMaxA;
-                        maxAStr = currentStr;
+                    String currentStr = queue.take();
+                    long currentMaxA = currentStr.chars().filter(x -> x == ch).count();
+                    if (currentMaxA > maxCh.get()) {
+                        maxCh.set(currentMaxA);
                     }
                     Thread.sleep(150);
-//                    System.out.println("Queue1 take()" + queue1);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-            }
+            };
+            System.out.println("Максимум " + ch + ": " + maxCh);
         });
-
-        Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
-                try {
-                    String currentStr = queue2.take();
-                    long currentMaxA = currentStr.chars().filter(ch -> ch == 'b').count();
-                    if (currentMaxA > maxB) {
-                        maxB = currentMaxA;
-                        maxBStr = currentStr;
-                    }
-                    Thread.sleep(150);
-//                    System.out.println("Queue2 take()" + queue2);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        Thread thread3 = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
-                try {
-                    String currentStr = queue3.take();
-                    long currentMaxA = currentStr.chars().filter(ch -> ch == 'c').count();
-                    if (currentMaxA > maxC) {
-                        maxC = currentMaxA;
-                        maxCStr = currentStr;
-                    }
-                    Thread.sleep(150);
-//                    System.out.println("Queue3 take()" + queue3);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        thread0.start();
-        thread1.start();
-        thread2.start();
-        thread3.start();
-
-        thread0.join();
-        thread3.join();
-        thread2.join();
-        thread1.join();
-
-
-        System.out.println("Максимум a " + maxA + ": " + maxAStr);
-        System.out.println("************************************************************************");
-        System.out.println("Максимум b " + maxB + ": " + maxBStr);
-        System.out.println("************************************************************************");
-        System.out.println("Максимум c " + maxC + ": " + maxCStr);
     }
 
     public static String generateText(String letters, int length) {
